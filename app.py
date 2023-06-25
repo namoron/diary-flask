4907498894b8f6bd1a2442b859fb2d49027f86ec
namor3 <213shirokumals530hl@gmail.com>
2023-06-25 20:16:04 +0900

    second

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello World!"
+@app.route("/resister")
+def resister():
+    return "resister"
+@app.route("/search")
+def search():
+    return "search"
